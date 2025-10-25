8597a26abdeae8f47c587866f0e85561b38b953d
sheepy <rainymausam111@gmail.com>
2025-10-25 16:09:25 +0530

    Computer Memoji placed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,31 @@
+import memojiImage from '@/assets/images/memoji-computer.png';
+import Image from 'next/image';
+import ArrowDown from '@/assets/icons/arrow-down.svg';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className='py-32'>
+      <div className="container">
+        <div className='flex flex-col items-center'>
+          <Image src={memojiImage} alt='Person behind laptop peeking at you' className='size-[100px]'></Image>
+          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
+            <div className='bg-green-500 size-2.5 rounded-full'></div>
+            <div className='text-sm font-medium'>Available for new projects</div>
+          </div>
+        </div>
+        <h1>Building exceptional User Experiences</h1>
+        <p>We specialize in transforming your problems into functional, high-performing systems. Let's discuss your next project</p>
+        <div>
+          <button>
+            <span>Explore our work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span>👋</span>
+            <span>Let's connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
