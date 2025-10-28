a0efdf336672bbf37ebed02807688eb426f4c6bf
sheepy <rainymausam111@gmail.com>
2025-10-28 16:12:09 +0530

    Contact Us section completed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,15 +1,25 @@
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+import grainImage from '@/assets/images/grain.jpg';
 
 export const ContactSection = () => {
-  return <div className='py-16'>
+  return <div className='py-16 pt-12 lg:py-24 lg:pt-20'>
     <div className='container'>
-      <div className=''>
-        <h2>Let's create something amazing together</h2>
-        <p>Ready to bring your next project to life? Let's connect and discuss how we can help you achieve your goals</p>
-        <button>
-          <span>Contact Us</span>
-          <ArrowUpRightIcon/>
-        </button>
+      <div className='bg-gradient-to-r from-[#10b96a] to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0'>
+        <div className='absolute inset-0 opacity-10 -z-10' style={{
+          backgroundImage: `url(${grainImage.src})`
+        }}></div>
+        <div className='flex flex-col md:flex-row gap-8 md:gap-16 items-center'>
+          <div>
+            <h2 className='font-serif text-2xl md:text-3xl'>Let's create something amazing together</h2>
+            <p className='text-sm mt-2 md:text-base'>Ready to bring your next project to life? Let's connect and discuss how we can help you achieve your goals</p>
+          </div>
+          <div>
+            <button className='text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900'>
+              <span className='font-semibold'>Contact Us</span>
+              <ArrowUpRightIcon className='size-4'/>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>;
